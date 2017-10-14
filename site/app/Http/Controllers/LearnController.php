b0dc210941a1de0752943abda8f67d33e68e6c66
Rodrigo Avellar de Muniagurria <roavellarm@gmail.com>
2017-10-14 18:01:45 -0300

    Site em Laravel com CRUD user - v. atual do servidor

---
+++ site/app/Http/Controllers/LearnController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Hash;
+
+class LearnController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data['users'] = User::all();
+        return view('users',$data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $user = [
+            'name' => $request->nome,
+            'email' => $request->email,
+            'password' => Hash::make($request->nome),
+        ];
+        $save = User::insert($user);
+        if($save)
+            return redirect('users');
+        else
+            return redirect()->back()->withInput();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data['user'] = User::find($id);
+        return view('create',$data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if($request->has('password')){
+            $password = $request->password;
+            $user = [
+                'name' => $request->nome,
+                'email' => $request->email,
+                'password' => $password,
+            ];
+        }
+        else{
+            $user = [
+                'name' => $request->nome,
+                'email' => $request->email,
+            ];
+        }
+        $update = User::find($id)->update($user);
+        if($update)
+            return redirect('users');
+        else
+            return redirect()->back()->withInput();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+        if($user){
+            $user->destroy($id);
+            $msg = 'Usuário removido com Sucesso.';
+        }
+        else{
+            $msg = 'Usuário não encontrado';
+        }
+        return redirect()
+            ->back()
+            ->withSucess($msg);        
+    }
+}
