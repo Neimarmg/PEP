fb2483964da7525678bb6da7c31f1f803b3fd337
AUMASA <aurelio.m.santana@hotmail.com>
2017-11-13 01:12:18 -0200

    Alteraçoes Treino formulario

--- site/resources/views/treino/formulario.blade.php
+++ site/resources/views/treino/formulario.blade.php
@@ -2,7 +2,38 @@
 
 @section('content')
 {{--  Colocar aqui o código  --}}
-CADASTRAR TREINOS
+
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4"></div>
+		<div class="col-md-4">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <label class="control-label">Nome</label>
+                    <br>
+                    <select class="form-control" id="id"></select>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label">Exercício</label>
+                    <br>
+                    <select class="form-control" id="id"></select>
+                </div>
+
+                <div class="form-group">                   
+                    <label class="control-label">Comnentários</label>
+                    <br>
+                    <textarea class="form-control" rows="5" id="comment"></textarea>
+                </div>
+            </form>
+		</div>
+		<div class="col-md-4"></div>
+	</div>
+</div>
+
+
+
 @endsection
 
 {{-- Posteriormente: Para fazer upload de imagem!!!
@@ -14,3 +45,4 @@ CADASTRAR TREINOS
             <input type="hidden" value="{{ csrf_token() }}" name="_token">
         </form>
   --}}
+
